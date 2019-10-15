14d1eb3a0eebcb1c4305536ef896de0b6a770fd3
Tommy Lai <whlai@avidxchange.com>
2019-10-15 13:36:22 -0600

    Add project files.

---
+++ BlazorBCPCLient/Data/BoardingData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBCPCLient.Data
+{
+    public class BoardingData
+    {
+        [Required]
+        [MaxLength(50)]
+        public string bankId { get; set; }
+
+        [Required]
+
+        public string bankName { get; set; }
+
+        public string bankDescription { get; set; }
+    }
+}
